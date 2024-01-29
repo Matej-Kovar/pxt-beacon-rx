f0de196ba2ed9a40ceac7854fecae30427e4fa5b
matej-kovar <146720349+Matej-Kovar@users.noreply.github.com>
2024-01-29 11:51:35 +0100

    save from yesterday

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+radio.setTransmitSerialNumber(true);
+radio.setTransmitPower(4);
+radio.setGroup(151)
+radio.onReceivedNumber(function(receivedNumber: number) {
+    let serialNum = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+    if(serialNum === 957673456){
+        basic.showNumber(receivedNumber);
+    }
+    
+})
+input.onButtonPressed(Button.A, function() {
+    basic.showNumber(control.deviceSerialNumber())
+})
